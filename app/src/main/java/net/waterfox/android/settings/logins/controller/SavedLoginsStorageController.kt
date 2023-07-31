ea78897a2be7a2846d99edf3fc2fb49b0adb01ae
Alex Kontos <MrAlex94@users.noreply.github.com>
2023-07-31 16:15:33 +0100

    [fenix] Updating the code to work with application-services v95.0.0
    This required updating the code to handle the new app-services error
    hierarchy.

--- app/src/main/java/net/waterfox/android/settings/logins/controller/SavedLoginsStorageController.kt
+++ app/src/main/java/net/waterfox/android/settings/logins/controller/SavedLoginsStorageController.kt
@@ -17,7 +17,7 @@ import kotlinx.coroutines.withContext
 import mozilla.components.concept.storage.Login
 import mozilla.components.concept.storage.LoginEntry
 import mozilla.components.service.sync.logins.InvalidRecordException
-import mozilla.components.service.sync.logins.LoginsStorageException
+import mozilla.components.service.sync.logins.LoginsApiException
 import mozilla.components.service.sync.logins.NoSuchRecordException
 import mozilla.components.service.sync.logins.SyncableLoginsStorage
 import net.waterfox.android.R
@@ -90,7 +90,7 @@ open class SavedLoginsStorageController(
         try {
             val encryptedLogin = passwordsStorage.add(loginEntryToSave)
             syncAndUpdateList(passwordsStorage.decryptLogin(encryptedLogin))
-        } catch (loginException: LoginsStorageException) {
+        } catch (loginException: LoginsApiException) {
             Log.e(
                 "Add new login",
                 "Failed to add new login.", loginException
@@ -139,7 +139,7 @@ open class SavedLoginsStorageController(
         try {
             val encryptedLogin = passwordsStorage.update(guid, loginEntryToSave)
             syncAndUpdateList(passwordsStorage.decryptLogin(encryptedLogin))
-        } catch (loginException: LoginsStorageException) {
+        } catch (loginException: LoginsApiException) {
             when (loginException) {
                 is NoSuchRecordException,
                 is InvalidRecordException -> {
@@ -190,7 +190,7 @@ open class SavedLoginsStorageController(
         val validEntry = if (entry.password.isNotEmpty()) entry else entry.copy(password = "password")
         var dupe = try {
             passwordsStorage.findLoginToUpdate(validEntry)?.mapToSavedLogin()
-        } catch (e: LoginsStorageException) {
+        } catch (e: LoginsApiException) {
             // If the entry was invalid, then consider it not a dupe
             null
         }
